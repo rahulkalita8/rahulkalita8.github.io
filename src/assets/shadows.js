475442f5a1faf85fe1ff54a5bba3b40bd3636557
Rahul Kalita <rkalita@paypal.com>
2021-05-09 22:08:37 +0530

    Updates

--- src/assets/shadows.js
+++
@@ -1,5 +0,0 @@
-export const levels = {
-    _l1: `box-shadow: 0 0 1px rgba(0,0,0,0.2);`,
-    _l2: `box-shadow: 0 0 10px rgba(0,0,0, 0.05), 0 2px 4px rgba(0,0,0, 0.1);`,
-    _l3: `box-shadow:  0 5px 15px rgba(0,0,0, 0.1), 0 2px 3px rgba(0,0,0, 0.1);`,
-}
